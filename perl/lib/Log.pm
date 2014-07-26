fdd6feed491f06c1643b85e6a9f519b359038543
windymelt <windy.imlovinit@gmail.com>
2014-07-26 23:16:39 +0900

    課題Perl-1実装。テストはGMT-JST関連の問題で不通過。解決待ち

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -2,24 +2,49 @@ package Log;
 use strict;
 use warnings;
 
+use DateTime;
+
 sub new {
     my ($class, %args) = @_;
+
+    my ($method, $path, $protocol) = split(/ /, $args{req});
+    $args{method} = $method;
+    $args{path} = $path;
+    $args{protocol} = $protocol;
+
     return bless \%args, $class;
 }
 
 sub protocol {
+    my $self = shift;
+
+    return $self->{protocol};
 }
 
 sub method {
+    my $self = shift;
+
+    return $self->{method};
 }
 
 sub path {
+    my $self = shift;
+
+    return $self->{path};
 }
 
 sub uri {
+    my $self = shift;
+
+    my $host = $self->{host};
+    my $path = $self->{path};
+    return "http://$host$path";
 }
 
 sub time {
+    my $self = shift;
+    my $dt = DateTime->from_epoch(epoch => $self->{epoch}, time_zone => 'Asia/Tokyo');
+    return $dt->strftime('%Y-%m-%dT%H:%M:%S');
 }
 
 1;
