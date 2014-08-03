d330166409a51c5a7219f31664cf61b376046b57
Momijikawa <windy.imlovinit@gmail.com>
2014-08-03 14:10:09 +0900

    Finished JS work #1.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
+function parseLTSVLog(logStr){
+    if(! logStr instanceof String){
+	throw new InvalidArgumentsException('Argument logStr must be String');
+    }
+    
+    // empty input, empty array
+    if(logStr == ""){ return []; }
 
+    var logStrLines = logStr.split("\n");
+    var parsedLogs = [];
+
+    logStrLines.forEach(function(aLine){
+	if(aLine != ''){
+	    var logHashTable = new Object();
+	    var tabSplitedElements = aLine.split("\t");
+
+	    tabSplitedElements.forEach(function(anElement){
+		// keyValuePair[0]:keyValuePair[1] == hoge:piyo
+		var keyValuePair = anElement.split(':');
+		
+		// when key equals 'reqtime_microsec', treat value as Int
+		if(keyValuePair[0] == 'reqtime_microsec'){
+		    logHashTable[keyValuePair[0]] = parseInt(keyValuePair[1]);
+		}else{
+		    logHashTable[keyValuePair[0]] = keyValuePair[1];
+		}
+	    })
+	    
+	    // push one hashtable into the array
+	    parsedLogs.push(logHashTable);
+	}})
+    return parsedLogs;
+}
 // 課題 JS-2: 関数 `createLogTable` を記述してください
