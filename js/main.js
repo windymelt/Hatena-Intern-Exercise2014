6fcc9836c0d7f0282d83b925f0c170f2384beea4
Momijikawa <windy.imlovinit@gmail.com>
2014-08-03 22:10:34 +0900

    Add error avoidance for testing

--- js/main.js
+++ js/main.js
@@ -53,12 +53,15 @@ function createLogTable(divDOM, parsedLTSV){
     console.log(divDOM.innerHTML);
 }
 
+// do only if 'submit-button' is defined to avoid an error when testing
+if(document.getElementById('submit-button')){
 // when submit-button is clicked
-document.getElementById('submit-button').addEventListener('click', function(){
-    var tableContainerDOM = document.getElementById('table-container');
-    var LTSVText = document.getElementById('log-input').value;
-    // clearing div for the table
-    tableContainerDOM.innerHTML = '';
-    createLogTable(tableContainerDOM, parseLTSVLog(LTSVText));
-});
+    document.getElementById('submit-button').addEventListener('click', function(){
+	var tableContainerDOM = document.getElementById('table-container');
+	var LTSVText = document.getElementById('log-input').value;
+	// clearing div for the table
+	tableContainerDOM.innerHTML = '';
+	createLogTable(tableContainerDOM, parseLTSVLog(LTSVText));
+    });
+}
 
