d5fe7fad3ac8e2c86a2d89b4a1674c808753d7f2
Momijikawa <windy.imlovinit@gmail.com>
2014-08-04 23:52:09 +0900

    implement visualizer

--- perl/lib/Visualizer.pm
+++ perl/lib/Visualizer.pm
@@ -4,7 +4,7 @@ use warnings;
 
 use DateTime;
 use Data::Dumper;
-use List::Util 'min', 'max';
+use List::Util 'min', 'max', 'sum';
 
 sub new {
   my ($class, %args) = @_;
@@ -18,21 +18,32 @@ sub meantime_for_path {
   my @logs = @{$self->{logs}};
   my %access_count = ();
   my $maximum = 0;
+  my $minimum = 0;
+
   foreach my $log (@logs) {
     push @{$access_count{$log->{path}}}, ($log->{reqtime_microsec});
     if($log->{reqtime_microsec} > $maximum){$maximum = $log->{reqtime_microsec};}
+    if($minimum == 0 || $log->{reqtime_microsec} < $minimum){$minimum = $log->{reqtime_microsec};}
   }
   #print Dumper(%access_count);
   foreach my $path (sort keys %access_count){
     my $bar_length = $maximum_length - $maximum_path_length;
-    my $from = (min(@{$access_count{$path}})  / $maximum) * $bar_length;
-    my $to = (max(@{$access_count{$path}}) / $maximum) * $bar_length;
+    my $path_min = min(@{$access_count{$path}});
+    my $path_max = max(@{$access_count{$path}});
+    my $from = ($path_min / $maximum) * $bar_length;
+    my $to = ($path_max / $maximum) * $bar_length;
     my @bar = split //, ('-' x $bar_length);
     for($from..$to){
       $bar[$_] = '=';
     }
-    printf "%${maximum_path_length}s%s\n", $path, join('', @bar);
+    my $avg = average(@{$access_count{$path}});
+    $bar[($avg / $maximum) * $bar_length] = 'A';
+    my $mean = mean(@{$access_count{$path}});
+    $bar[($mean / $maximum) * $bar_length] = 'M';
+    printf "%${maximum_path_length}s%s%s\n", $path, join('', @bar), "$path_min - $path_max, AVG $avg, MEAN $mean";
   }
+  print "-----visualize status-----";
+  printf "MIN: %d MAX: %d \n", $minimum, $maximum;
 }
 
 sub average {
