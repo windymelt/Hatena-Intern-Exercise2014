68793fffe45932e0ed57097ddc6839585455efdd
Momijikawa <windy.imlovinit@gmail.com>
2014-08-05 20:22:50 +0900

    refactor perl/lib/LogCounter.pm

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -10,22 +10,10 @@ sub new {
 sub group_by_user {
   # logs held by this instance
   my $ref_logs_arr = shift()->{logs};
-
   my %user_grouped_logs;
   
   # process each log one by one
-  foreach my $log (@$ref_logs_arr) {
-    if (! defined($log->{user})) {
-      # if log has no 'user' key, regard 'user' field as 'guest'
-      my $log_array = $user_grouped_logs{guest};
-      push(@$log_array, ($log));
-      $user_grouped_logs{guest} = $log_array;
-    } else {
-      my $log_array = $user_grouped_logs{$log->{user}};
-      push(@$log_array, ($log));
-      $user_grouped_logs{$log->{user}} = $log_array;
-    }
-  }
+  foreach my $log (@$ref_logs_arr) { push(@{$user_grouped_logs{$log->{user} // 'guest'}}, ($log)); }
   return \%user_grouped_logs;
 }
 
