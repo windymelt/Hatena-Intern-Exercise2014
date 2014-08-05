1f195f6ae60756c0316e45a1e6e890262af89bd0
Momijikawa <windy.imlovinit@gmail.com>
2014-08-05 20:48:54 +0900

    Refactor perl/lib/Parser.pm

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -9,35 +9,23 @@ use Log;
 sub new {
     my ($class, %args) = @_;
     return bless \%args, $class;
-}
+  }
 
 sub parse {
     my $self = shift;
-
+    my @logs = ();
     # read log file
     open my $LOGFILE, '<', $self->{filename} or die $!;
-    my @lines_of_log = <$LOGFILE>;
-    close $LOGFILE;
-
-    # choose only parsable lines
-    my @parsable_lines_of_log = grep { /(?!\s)/ } @lines_of_log;
-
-    # parse each line as Log
-    my @logs = map {
+    while (defined ($_ = <$LOGFILE>)){
+      # choose only parsable lines
+      (! /^(?!\s)$/) || next;
       chomp;
       # split line separated by TAB
-      my @splited = split /\t/;
-      # exclude chunk which has 'MINUS' sign as value e.g. hogehoge:-
-      my @filtered = grep { !/\S+:-$/ } @splited;
-      # make hash spliting by COLON
-      my @hash = map { split m{:(?!//)} } @filtered;
-
-      # this is an element for a line
-      Log->new(@hash);
-    } @parsable_lines_of_log;
-    
+      push @logs, Log->new(map { split m{:(?!//)} } grep { !/\S+:-$/ } split /\t/);
+    }
+    close $LOGFILE;
     \@logs;
-}
+  }
 
 
 1;
